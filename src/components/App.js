a948900d835c66c4fdbea8f5d6bd5b3bb00fe232
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-06-10 23:44:05 +0300

    change props children at App

--- src/components/App.js
+++ src/components/App.js
@@ -50,44 +50,38 @@ function App() {
           isOpen = {isEditProfilePopupOpen}
           onClose = {closeAllPopups}
           name = 'edit-profile'
-          title = 'Редактировать профиль'
-          children = {
+          title = 'Редактировать профиль'>
             <>
               <input className="popup__input popup__input_text_name" id="firstname" name="firstname" placeholder="Имя" type="text" required minLength="2" maxLength="40"/>
               <span className="popup__error" id="firstname-error"></span>
               <input className="popup__input popup__input_text_about" id="about" name="about" placeholder="О себе" type="text" required minLength="2" maxLength="200"/>
               <span className="popup__error" id="about-error"></span>
             </>
-          }
-        />
+        </PopupWithForm>
 
         <PopupWithForm 
           isOpen = {isEditAvatarPopupOpen}
           onClose = {closeAllPopups}
           name = 'edit-avatar'
-          title = 'Обновить аватар'
-          children = {
+          title = 'Обновить аватар'>
             <>
               <input className="popup__input" id="avatarlink" name="url" type="url" placeholder="Ссылка на аватар" required />
               <span className="popup__error" id="avatarlink-error"></span>
             </>
-          }
-        />
+        </PopupWithForm>
 
         <PopupWithForm 
           isOpen = {isAddPlacePopupOpen}
           onClose = {closeAllPopups}
           name = 'add-card'
-          title = 'Новое место'
-          children = {
+          title = 'Новое место'>
             <>
               <input className="popup__input popup__input_text_name-place" id="nameplace" name="name" placeholder="Название" type="text" required minLength="2" maxLength="30"/>
               <span className="popup__error" id="nameplace-error"></span>
               <input className="popup__input popup__input_text_image-link" id="imagelink" name="link" placeholder="Ссылка на картинку" required type="url"/>
               <span className="popup__error" id="imagelink-error"></span>
             </>
-          }
-        />
+        </PopupWithForm>
 
         <PopupWithForm 
           name = 'confirmation'
