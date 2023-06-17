aa0ce34004df8fab85f2ebd7ca5c44c9bf0da58f
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-06-17 18:17:24 +0300

    add const currentUser

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
+import React, { useEffect } from 'react';
+
+import api from '../utils/api';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup.js';
-import React from 'react';
 
 function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false)
   const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false)
   const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false)
   const [selectedCard, setSelectedCard] = React.useState(null)
+
+  const [currentUser, setCurrentUser] = React.useState({})
+
+  useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()]).then(([profileInfo, card]) => {
+      setCurrentUser(profileInfo);
+      setCards(card)
+    }).catch((err) => {
+      console.error(err);
+    })
+  }, [])
 
   function handleEditProfileClick() {
     setEditProfilePopupOpen(true);
