d1eab91a9f3e1003ad29d5c6f402d56c5afbffb6
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-06-17 18:51:24 +0300

    add context card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,31 @@
+import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 function Card({card, onCardClick}) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = card.owner._id === currentUser._id;
+  // Далее в разметке используем переменную для условного рендеринга
+  const cardDeleteButtonClassName = (`places__delete-card ${!isOwn && 'places__delete-card_hidden'}`)
+
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = (`places__like ${isLiked && 'places__like_active'}`);
+
   function handleClick() {
     onCardClick(card);
   }
 
+  function handleLikeClick() {
+    onCardLike(card);
+  }
+
+  function handleConfirmClick() {
+    onConfirmCardDelete(card);
+  }
+
   return (
       <li className="places__item">
-        <button type="button" className="places__delete-card"></button>
+        <button type="button" className={cardDeleteButtonClassName}></button>
         <div onClick={handleClick} className="places__image-container">
           <img src={card.link} alt={card.name} className="places__image"/>
         </div>
